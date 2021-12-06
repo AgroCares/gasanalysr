bcd4130dd2a0d737648c2a25e7b82b52a19f0c46
Brent <brent.riechelman@xs4all.nl>
2021-12-06 10:43:51 +0100

    Dev ppm converter

--- dev/dev_ppm_converter.R
+++ dev/dev_ppm_converter.R
@@ -1,3 +1,4 @@
+library(data.table)
 dtm <- read.delim('inst/extdata/mgm3_25c.meas', skip = 6) |> setDT()
 dtp <- read.delim('inst/extdata/ppm.meas', skip = 6) |> setDT()
 
@@ -25,4 +26,30 @@ ddtt <- dcast(dtt, timestamp + gas + molarmass ~ c, value.var = 'value')
 ddtt[,t1 := 0.0409*ppm*molarmass]
 
 # calculate verhouding
-ddtt[,rat := mgm3/ppm]
+ddtt[,rat := round(mgm3/ppm,3)]
+
+# calculate difference between calculated and measured mgm3
+ddtt[,dif_t1 := mgm3-t1]
+# calculated difference percentage
+ddtt[,dif_t1_fr := dif_t1/mgm3*100]
+
+# calculate R for 25C at 1 atm  with V = RT/P
+# molar gas constant
+mgc <- 8.314462618
+tk <- 298.15
+P <- 1*1.01325*10^5 #(convert atm to Pa)
+
+R <- (mgc*tk)/P
+
+test <- P/(0.08206*298)
+P*1/mgc/tk
+
+# the ideal gas law is PV = nRT
+# where P is pressure (Pa)
+# V = volumne
+# n = number of moles
+# R = universal gas constant (8.314462618)
+# T = temperature in Kelvin
+#
+# to get the conversion factor 0.0409 above == P*V/R/T/1000 = mmol/m3
+cv <- P*1/mgc/tk/1000
