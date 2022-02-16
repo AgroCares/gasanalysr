9025f76378b2bacf42f7c7cdd474a016150f82ff
Brent <brent.riechelman@xs4all.nl>
2022-02-16 10:38:56 +0100

    add test on measurement column names with mgm3

--- tests/testthat/test-preprocessing.R
+++ tests/testthat/test-preprocessing.R
@@ -1,3 +1,4 @@
+require(data.table)
 test_that('preprocessing samplekey works', {
   # load data
   load(paste0(system.file("testdata", package = 'gasanalysr'),
@@ -6,12 +7,30 @@ test_that('preprocessing samplekey works', {
   expect_equal(ppr_samplekey(dtkey), ndtkey)
 })
 
-test_that('preprocessing of measurements works', {
+test_that('preprocessing of measurements works with units in ppm', {
   # load data
   load(paste0(system.file("testdata", package = 'gasanalysr'),
               '/pprmeasurementtest.RData'))
   # test
   expect_equal(ppr_measurement(measurementsdt), nmeasdt)
+})
+
+test_that('name formatting in ppr measurement works for units in mgm3', {
+  # make data
+  tdt <- data.table(
+    Timestamp ='2021-11-17 09:44:39',
+    CO2..mg.m3. = 900,
+    H2O..mg.m3. = 1500,
+    N2O..mg.m3. = 0.86,
+    NH3..mg.m3. = 0.331,
+    inlet = 1,
+    x = NA
+  )
+
+  # test
+  expect_equal(names(ppr_measurement(tdt)), c('Timestamp', 'co2.mgm3',
+                                                   'h2o.mgm3', 'n2o.mgm3',
+                                                   'nh3.mgm3'))
 })
 
 # test that conversion for co2 works
