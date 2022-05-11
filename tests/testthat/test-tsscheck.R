8f130f26147bd319aa217bcff044fe18c34c50bb
Brent <brent.riechelman@xs4all.nl>
2022-05-11 11:53:13 +0200

    improve testdata path

--- tests/testthat/test-tsscheck.R
+++ tests/testthat/test-tsscheck.R
@@ -1,9 +1,9 @@
 # test tsscheck
-path <- if(length(list.files("../testthat/"))>0){'../../'} else {'.'}
+path <- if(length(list.files("../testthat/"))>0){'../testdata/'} else {'tests/testdata/'}
 
 test_that('seal time is not thrown out upon tsscheck', {
   cat(getwd())
-  dtm <- readRDS('tests/testdata/tsscheck_testdat1.RDS')
+  dtm <- readRDS(paste0(path,'tsscheck_testdat1.RDS'))
   dtmt <- tsscheck(dtm = dtm)
   expect_equal(dtmt[!is.na(startend)], dtmt[!is.na(stimedate)])
 })
