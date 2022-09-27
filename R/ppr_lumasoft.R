71bcdb10c6cab230fa1f5b07945d83b1d085fb4c
Brent <brent.riechelman@xs4all.nl>
2022-09-27 09:51:05 +0200

    replace path by p and function to convert xlsx to dt

--- R/ppr_lumasoft.R
+++ R/ppr_lumasoft.R
@@ -2,25 +2,27 @@
 #'
 #' A function to turn a .xls file exported from Lumasoftgas 7820 or 7880 into a usable data.table or csv
 #'
-#' #' @param path (character) Path to the file
+#' #' @param p (character) Path to a directory containing xls files to convert
 #'
 #' @import data.table
 #'
 #' @examples
 #'
-ppr_lsg <- function(path){
+ppr_lsg <- function(p){
   # add visual binding
 
   # check inputs
-  checkmate::assert_character(path, n.chars = 5)
-  # checkmate::assert_true(grepl('\\.xls$', path)) # not required of operating on folder
+  checkmate::assert_character(p, min.chars = 5)
+  checkmate::assert_directory(p)
 
   # copy files to new folder and get path to new folder
-  np <- lsg_fileppr(path)
+  np <- lsg_fileppr(p)
 
   # change file extensions from xls to xlsx
   file.rename(from = list.files(np, full.names = TRUE),
               to = gsub('\\.xls$', '\\.xlsx', list.files(np, full.names = TRUE)))
+
+  #
 
 }
 #' Copy lsg raw data into new location
@@ -31,14 +33,11 @@ lsg_fileppr <- function(p){
 
   # check inputs
 
-  # get folder location
-  fl <- dirname(p)
-
   # make new folder location
-  dir.create(file.path(fl, 'ppr_lsg'))
+  dir.create(file.path(p, 'ppr_lsg'))
 
   # select files to copy
-  filestocopy <- list.files(fl, full.names = TRUE)
+  filestocopy <- list.files(p, full.names = TRUE)
 
   # copy files into newdir
   file.copy(from = filestocopy, to = file.path(fl, 'ppr_lsg'),
@@ -46,4 +45,43 @@ lsg_fileppr <- function(p){
 
   # return new dir
   return(paste0(fl, '/ppr_lsg'))
+}
+#' Read lsg file and turn into formatted dt
+#'
+#' @param f (character) file name
+#' @param p (character) path to directory file is in
+lsg_xlsx_to_dt <- function(f, p){
+  # add visual binding
+  floc = dt = mdt = out = sn = sts = NULL
+
+  # check inputs
+  checkmate::assert_directory(p)
+  checkmate::assert_file_exists(paste0(p, '/', f))
+
+  # get full file location
+  floc <- paste0(p, '/',f)
+
+  # get sheet names
+  sts <- readxl::excel_sheets(floc)
+
+  for(sn in sts[-1]) {
+    # read sheet
+    dt <- readxl::read_excel(path = floc, sheet = sn) |> setDT()
+
+    # melt
+    mdt <- melt(dt, id.vars = "Date & Time")
+
+    # set value column name
+    setnames(mdt, old = 'value', sn)
+
+    # merge in product
+    if(!exists('out')){
+      out <- mdt
+    } else{
+      out <- merge(out, mdt)
+    }
+  }
+
+  # return
+  return(out)
 }
